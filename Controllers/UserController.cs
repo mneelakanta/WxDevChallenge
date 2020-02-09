c4762506793c678bdafff566b0c26ec2d6061c86
mouna.neelakanta <mouna.neelakanta@myob.com>
2020-02-09 16:49:27 +1100

    add skeleton code

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WxDevChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public User Get()
+        {
+            return new User {Name = "Mouna Neelakanta", Token = "67b75a66-1654-4332-ba4f-85b095ef235a"};
+        }
+    }
+}
